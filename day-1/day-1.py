ce699d9a69e3129ed0dc41ed66862a13e23a184d
draav <devlin.nicholas.j@gmail.com>
2021-12-08 14:21:08 -0500

    finished day 1

--- day-1/day-1.py
+++ day-1/day-1.py
@@ -1,3 +1,17 @@
-with open('readme.txt') as f:
-    lines = f.readlines()
-print(lines)
+with open('input.txt') as f:
+    previous = int(f.readline())
+    count = 0
+    increasing_measurements = 0
+    print(previous)
+    for line in f:
+        # if count > 5:
+        #     break
+        
+        current = int(line)
+        if current > previous:
+            increasing_measurements += 1
+        previous = current
+        print(previous)
+        count += 1
+
+print(increasing_measurements)
